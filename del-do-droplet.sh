d142169fc9a339726f69c3fc8b14041d76e86393
Robert Rennison <robrennison@gmail.com>
2016-03-21 10:22:25 -0400

    leff of del-do-droplet.sh from our commit

--- del-do-droplet.sh
+++ del-do-droplet.sh
@@ -3,6 +3,6 @@ DROPNAME=$1
 TOKEN_FILE=~/bin/digital-ocean-env.sh
 source $TOKEN_FILE
 echo $0 $1 $2
-#ansible-playbook  new-do-droplet.yml --extra-vars '"droplets":["$DROPNAME"]'
-#echo "droplets":["${DROPNAME}"]
+# Note the tricky use of terminating the single quote so that the S{DROPNAME}
+# can be expanded. The quotes are then restarted
 ansible-playbook -i digital_ocean.py  new-do-droplet.yml --extra-vars '{"droplets":['"${DROPNAME}"'], "desired_state":"deleted"}'
