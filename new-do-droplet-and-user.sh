1c21b5cb06c86c132897fff7ff2e7ace2958eed8
Robert Rennison <robrennison@gmail.com>
2016-04-14 13:47:47 -0400

    Updated the version of docker-compose to 1.7. Now docker-compose log works as advertised

--- new-do-droplet-and-user.sh
+++ new-do-droplet-and-user.sh
@@ -2,12 +2,14 @@
 DROPNAME=$1
 TOKEN_FILE=~/bin/digital-ocean-env.sh
 source $TOKEN_FILE
-ansible-playbook  new-do-droplet.yml --extra-vars="droplets=${DROPNAME}"
+ansible-playbook  new-do-droplet.yml --extra-vars="droplets=${DROPNAME}" -vv
 
-# Hack but otherwise I'm down for combininig the playbooks
+# Hack to avoid occasional failure of the new-user-script. Could presumably
+# fix by making one integrated playbook. For the moment ignore since soo much
+# time is spent mucking about with ansible foibles. 
 #echo "Sleeping for 10s"
 #sleep 10
 
-ansible-playbook -i digital_ocean.py new-user-roles.yml --extra-vars="hosts=${DROPNAME}" -vv
+ansible-playbook -i digital_ocean.py new-user-roles.yml --extra-vars="hosts=${DROPNAME}" -vvv
 
 ./digital_ocean.py
