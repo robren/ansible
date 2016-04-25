2c2318a222a7e69b9417168d7ce3f7f8593a4cf3
Robert Rennison <robrennison@gmail.com>
2016-04-25 18:42:12 -0400

    - Added a new shell script to print out my Digital ocean inventory - Modified the installation details for docker-compose, now uses the method   where docker-compose is pulled in as a binary and not the compose as a   container appraoch. This compose container was buggy and could not find   docker-compose.yl files sitting in the current directory. Arrgh, wasted many   hours.

---
+++ myinventory.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+source $HOME/bin/digital-ocean-env.sh
+python digital_ocean.py
+
--- new-do-droplet-and-user.sh
+++ new-do-droplet-and-user.sh
@@ -1,15 +1,22 @@
 #! /bin/bash -x
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 desired-host-name" >&2
+    exit 1
+fi
 DROPNAME=$1
 TOKEN_FILE=~/bin/digital-ocean-env.sh
 source $TOKEN_FILE
 ansible-playbook  new-do-droplet.yml --extra-vars="droplets=${DROPNAME}" -vv
 
-# Hack to avoid occasional failure of the new-user-script. Could presumably
+# Hack to avoid occasional failure of the new-user-script. Creation returns
+# success but ssh problems when immediately sshing in.  Could presumably
 # fix by making one integrated playbook. For the moment ignore since soo much
-# time is spent mucking about with ansible foibles. 
-#echo "Sleeping for 10s"
-#sleep 10
+# time is spent 'mucking' about with ansible foibles. 
+echo "Sleeping for 10s"
+sleep 10
 
 ansible-playbook -i digital_ocean.py new-user-roles.yml --extra-vars="hosts=${DROPNAME}" -vvv
 
+# Prints out the inventory of machines we have on Digital Ocean
 ./digital_ocean.py
